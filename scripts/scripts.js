c20d349c0ce95a03ab00209e4fc03d646fab0c47
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2025-09-19 11:25:13 -0400

    Create scripts.js
    I made a jQuery for image to consume the header window space.

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+/*
+    Name: Iamni Hollie
+    Date: September 19, 2025
+*/
+
+// jQuery for image to consume the header window space
+$(document).ready(function () {
+  $('.hero').height($(window).height());
+});
