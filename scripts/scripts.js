4f68ab4cafe3feddab1566cbd2c515f226de9460
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2025-09-19 12:43:40 -0400

    Create scripts.js for /root branch
    Moved the scripts folder to the /root branch directory.

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+/*
+  Name: Imani Hollie
+  Date: september 19, 2025
+*/
+
+// jQuery for image to consume the header window space
+$(document).ready(function(){
+  $('.hero').height($(window).height());
+});
