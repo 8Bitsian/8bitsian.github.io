22246cb9844fdf55598bcbeaf505def45263e090
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2025-09-19 12:44:19 -0400

    Delete scripts/scripts.js from main branch
    I moved the scripts folder to the /root branch.

--- scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-/*
-    Name: Iamni Hollie
-    Date: September 19, 2025
-*/
-
-// jQuery for image to consume the header window space
-$(document).ready(function () {
-  $('.hero').height($(window).height());
-});
